7c6fb97d7d470d385afee50b4f87ad6db4bd693e
Peter Weber <peter-aalen@gmx.net>
2023-04-03 10:56:07 +0200

    SWITCH|CASE 1

--- script_01.js
+++ script_01.js
@@ -14,11 +14,11 @@ isJohnOlder = (ageJohn > ageMark);
 isJohnEqual = (ageJohn == ageMark);
 
 // Ausgabe
-console.log("ageJohn: " + ageJohn);
-console.log("ageMark: " + ageMark);
-console.log("isJohnOlder: " + isJohnOlder);
-console.log("isJohnEqual: " + isJohnEqual);
-console.log("------------------");
+// console.log("ageJohn: " + ageJohn);
+// console.log("ageMark: " + ageMark);
+// console.log("isJohnOlder: " + isJohnOlder);
+// console.log("isJohnEqual: " + isJohnEqual);
+// console.log("------------------");
 
 /************ IF  ************/
 // TINA --> There is no alternative!
@@ -35,3 +35,68 @@ console.log("------------------");
 /************ IF - ELSE  ************/
 // mit Alternative  
 // entweder ja oder nein
+
+// if (false)
+// if(ageJohn > ageMark)
+// {
+//     console.log("John ist älter.");
+// } else 
+// {
+//     console.log("John ist jünger.");
+// }
+
+
+/************  Ternäre (ternary) Schreibweise ************/
+// console.log( (isJohnOlder) ? "John is older" : "Mark is older");
+
+
+/************ IF - ELSE IF  ************/
+// mit alternativen Fällen (älter, jünger, gleich alt)
+
+// if (isJohnOlder) 
+// {
+//     console.log("John ist älter.");
+// }
+// // 1. Alternative
+// else if(isJohnEqual)
+// {
+//     console.log("John ist gleich alt.")
+// }
+// // Wenn alle vorherigen Tests fehlschlagen
+// else 
+// {
+//     console.log("John ist jünger.");    
+// }
+
+/****** Fallunterscheidung / SWITCH|CASE 1 ******/
+
+const firstName = "Jane";
+let job;
+
+
+job = "driver";         // .. fährt TAXI! / UBER
+job = "diver";          // .. taucht im Rhein! 
+job = "artist";         // .. malt ein Bild!
+job = "pilot";          // .. macht etwas anderes! --> default
+job = "teacher";        // .. unterrichtet!
+job = "instructor";     // .. unterrichtet!
+
+switch (job) {
+    case "driver":
+        console.log(firstName + " fährt TAXI!");
+        break;
+    case "diver":
+        console.log(firstName + " taucht im Rhein!");
+        break;
+    case "artist":
+        console.log(firstName + " malt ein Bild!");
+        break;
+    case "teacher":
+    case "instructor":
+        console.log(firstName + " erklärt wie man Kinder schlägt!");
+        break;
+
+    default:
+        console.log(firstName + " ist ARBEITSLOS");
+        break;
+}
